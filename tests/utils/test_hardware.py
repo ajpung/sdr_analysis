63b40936ab904b6626359e40073868334c7eb07c
ajpung <83614749+ajpung@users.noreply.github.com>
2024-01-22 09:13:43 -0700

    chore: Trying fixes for the RTLSDR package

--- tests/utils/test_hardware.py
+++ tests/utils/test_hardware.py
@@ -1,13 +1,11 @@
 from rtlsdr import RtlSdr
 
-sdr = RtlSdr()
-
-
 def test_data_read():
+    sdr = RtlSdr()
     # configure device
     sdr.sample_rate = 2.048e6  # Hz
-    sdr.center_freq = 70e6  # Hz
-    sdr.freq_correction = 60  # PPM
-    sdr.gain = "auto"
+    sdr.center_freq = 70e6     # Hz
+    sdr.freq_correction = 60   # PPM
+    sdr.gain = 'auto'
     samples = sdr.read_samples(512)
     assert len(samples) == 512

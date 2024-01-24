76f5ff7658926ff25f9a10e757c535a8a176eb3d
ajpung <83614749+ajpung@users.noreply.github.com>
2024-01-24 07:34:22 -0700

    chore: adding sample data for future test development

--- src/tada/utils/conversions.py
+++ src/tada/utils/conversions.py
@@ -1,10 +1,10 @@
 import numpy as np
-from typing import overload, Union
 
+from scipy.signal import welch
+from typing import overload, Union
 from tada.physics.constants import SPEED_OF_LIGHT
 
 
-'''
 @overload
 def freq_to_wvln(freq: float) -> float:
     ...
@@ -72,4 +72,34 @@ def db_to_decimal(db: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
     """
     dm = 10 ** (db / 10) - 1
     return dm
+
+
+'''
+def power_to_spectrum(pwr: np.ndarray) -> np.ndarray:
+    """
+    Standard conversion from power to frequency array
+
+    Args:
+        pwr - Extracted radio signal 
+    Returns:
+        freq_arr - Converted frequency arrat
+    """
+    #signal = []
+    sdr.sample_rate = 2.8e6
+    sdr.center_freq = 93.3e6
+
+    samples = sdr.read_samples(4000)
+    #signal.append(samples)
+    corr = 1.5
+    sample_freq, power = welch(samples, fs=sdr.sample_rate, window="hann", nperseg=2048, scaling="spectrum")
+    sample_freq = fftshift(sample_freq)
+    power = fftshift(power)/corr
+    print(sum(power))
+
+    plt.figure(figsize=(9.84, 3.94))
+    plt.plot((sample_freq+sdr.center_freq)/1e6, np.log10(power),linewidth=1)
+    plt.xlabel("Frequency (MHz)")
+    plt.ylabel("Relative power (dB)")
+    plt.show()    
+    assert False
 '''
